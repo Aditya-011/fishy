c552bcf845d747df806ae79f7777d1f1e43d7c98
Aditya-011 <adihgr18@gmail.com>
2022-02-14 15:15:59 +0530

    skip round added

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,65 +1,75 @@
 import React, { useContext, useState, useEffect } from "react";
 import "./Timer.css";
 import { useTimer } from "react-timer-hook";
-import { AuthContext } from "../../context/context";
+import { AuthContext, CodeContext, UserContext } from "../../context/context";
 import Icons from "../Icons/Icons";
 import Pause from "../../images/pause.png";
 import Resume from "../../images/resume.png";
 import { database as db } from "../../firebase";
-import { get, child, ref, onValue,update } from "firebase/database";
+import { get, child, ref, onValue, update } from "firebase/database";
+import useFirebaseRef from "../useFirebaseRef";
+import { useNavigate } from "react-router-dom";
 
-function MyTimer({ expiryTimestamp, auth, toggle, code }) {
+function MyTimer({ expiryTimestamp, toggle, code, round }) {
+  const navigate = useNavigate();
+  const { authUser } = useContext(AuthContext);
+  const { user } = useContext(UserContext);
+  const userID = user.uid
+  const path = authUser
+    ? `sessionData/${code}/hostProperties`
+    : `sessionData/${code}/state/${round}/${userID}`;
+  //  console.log(path);
+  const [roundData, loading] = useFirebaseRef(path);
+  const expiryHandler = () => {
+    if (authUser && !loading) {
+      const updates = {};
+      updates[path] = { ...roundData, isOver: true };
+      update(ref(db), updates);
+      navigate(`/host/results/${round}`);
+    } else {
+      if (!loading) {
+        const updates = {};
+        updates[path] = {
+          ...roundData,
+          isSubmit: {
+            choice: 1,
+            status: true,
+          },
+        };
+        update(ref(db), updates);
+        navigate(`/player/results/${round}`);
+      }
+    }
+  };
   const { seconds, minutes, isRunning, start, pause, resume, restart } =
-  useTimer({
-    expiryTimestamp,
-    onExpire: () => console.warn("onExpire called"),
-  });
+    useTimer({
+      expiryTimestamp,
+      onExpire: () => {
+        console.log("onExpire called");
+        expiryHandler();
+      },
+    });
   const [stop, setStop] = useState(false);
   useEffect(() => {
-    
-   
+    console.log(user);
     getPlayStatus();
     console.log(`timer status ${stop}`);
-  
   }, []);
-  useEffect(() => {
-  
-  }, [stop]);
 
-  const updateStopTimer=()=>
-  {
-    get(child(ref(db),`sessionData/${code}/hostProperties`)).then((snapshot) => {
-      if (snapshot.exists()) {
-        const res = snapshot.val()
-        const updates = {};
-        updates[`sessionData/${code}/hostProperties`] = {...res,stopTimer:!stop};
-        update(ref(db), updates)
-       
-      } else {
-        console.log("No data available");
-      }
-    }).catch((error) => {
-      console.error(error);
-    });
-  }
   const getPlayStatus = () => {
     const starCountRef = ref(db, `sessionData/${code}`);
     onValue(starCountRef, (snapshot) => {
       const res = Object.values(snapshot.val());
-      setStop(res[0].stopTimer)
-    
-    
-      //console.log(res[0].stopTimer);
+      setStop(res[0].stopTimer);
     });
   };
-
 
   return (
     <div style={{ textAlign: "center" }}>
       <div style={{ fontSize: "100px" }}>
         <span>{minutes}</span>:<span>{seconds}</span>
       </div>
-      {auth ? (
+      {authUser ? (
         <>
           {" "}
           {/*toggle.toggle ? (
@@ -89,12 +99,11 @@ function MyTimer({ expiryTimestamp, auth, toggle, code }) {
 }
 
 const Timer = ({ timer, round }) => {
-  const { auth } = useContext(AuthContext);
   const [time, setTime] = useState(new Date());
   const [toggle, settoggle] = useState(true);
   // 10 minutes timer
-  console.log(auth);
-  const code = sessionStorage.getItem("code");
+  // console.log(auth);
+  const { code } = useContext(CodeContext);
   const getTime = () => {
     get(child(ref(db), `sessionData/${code}/hostProperties`))
       .then((snapshot) => {
@@ -122,9 +131,9 @@ const Timer = ({ timer, round }) => {
     <div>
       <MyTimer
         expiryTimestamp={time}
-        auth={auth}
         code={code}
         toggle={{ toggle, settoggle }}
+        round={round}
       />
     </div>
   );
--- src/screens/Waiting/Waiting.js
+++ src/screens/Waiting/Waiting.js
@@ -1,197 +1,227 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import Button from '../../components/Button/Button';
+import React, { useContext, useEffect, useRef, useState } from "react";
+import Button from "../../components/Button/Button";
 
-import './Waiting.css';
-import { AuthContext, CodeContext } from '../../context/context';
-import { useNavigate } from 'react-router-dom';
-import { set, ref, update, get, child, onValue } from 'firebase/database';
-import { database as db } from '../../firebase';
-import useFirebaseRef from '../../components/useFirebaseRef';
+import "./Waiting.css";
+import { AuthContext, CodeContext } from "../../context/context";
+import { useNavigate } from "react-router-dom";
+import { set, ref, update, get, child, onValue } from "firebase/database";
+import { database as db } from "../../firebase";
+import useFirebaseRef from "../../components/useFirebaseRef";
+import { useTimer } from "react-timer-hook";
+
 const Waiting = () => {
-	const navigate = useNavigate();
-	const [roundNo, setRoundNo] = useState(0);
-	const [timeFormat, setTimeFormat] = useState('03:00');
-	const timerRef = useRef();
-	const [active, setActive] = useState(false);
-	const [counter, setCounter] = useState(180);
-	const [waitingMsg, setWaitingMsg] = useState('');
+  const navigate = useNavigate();
+  const [roundNo, setRoundNo] = useState(0);
+  const [timeFormat, setTimeFormat] = useState("03:00");
+  const timerRef = useRef();
+  const [active, setActive] = useState(false);
+  const [counter, setCounter] = useState(180);
+  const [waitingMsg, setWaitingMsg] = useState("");
+  const { authUser } = useContext(AuthContext);
+  const { code } = useContext(CodeContext);
 
-	const { authUser } = useContext(AuthContext);
-	const { code } = useContext(CodeContext);
+  const [sessionData, loading] = useFirebaseRef(`sessionData/${code}`);
+  const time = new Date();
+  const [player, loading1] = useFirebaseRef(`sessions/${code}/users`);
+  useEffect(() => {
+    //
 
-	const [sessionData, loading] = useFirebaseRef(`sessionData/${code}`);
-
-	const getRoundNo = () => {
-		get(child(ref(db), `sessionData/${code}/state`))
-			.then((snapshot) => {
-				if (snapshot.exists()) {
-					const res = Object.keys(snapshot.val());
-					console.log(res[res.length - 1]);
-					setRoundNo(Number(res[res.length - 1]) + 1);
-				} else {
-					console.log('No data available');
-				}
-			})
-			.catch((error) => {
-				console.error(error);
-			});
-	};
-
-	const movetoNextRound = () => {
-		if (
-			sessionData &&
-			sessionData.hostProperties.nextRound &&
-			sessionData.hostProperties.isOver
-		) {
-			const next = Object.keys(sessionData.state).length + 1;
-			console.log(next);
-			navigate(`/round/${next}`);
-		}
-	};
-	useEffect(() => {
-		//console.log(auth);\
-		getRoundNo();
-		if (!authUser && !loading) {
-			movetoNextRound();
-		}
-		console.log(roundNo);
-		/*sessionStorage.removeItem("time-val");
-    sessionStorage.removeItem("time-format");
-    sessionStorage.removeItem("time-percent");
-    if (sessionStorage.getItem("time-forma"))
-      setTimeFormat(sessionStorage.getItem("time-format"));
-    if (sessionStorage.getItem("active"))
-      setActive(JSON.parse(sessionStorage.getItem("active")));
-    if (sessionStorage.getItem("counter"))
-      setCounter(Number(sessionStorage.getItem("counter")));
-    socket.emit("join-game", sessionStorage.getItem("game-code"));
-    socket.on("next-round-started", roundNumber => {
-      window.location.href = `/round/${roundNumber}`;
-    });
-    socket.on("round-number", roundNumber => setRoundNo(roundNumber));
-    socket.on("message", ({ message }) => setWaitingMsg(message));
-    socket.on("game-over", () => (window.location.href = "/gameover"));
-    if (active) {
-      if (counter > 0) {
-        timerRef.current = setInterval(() => {
-          const secondCounter = counter % 60;
-          const minuteCounter = Math.floor(counter / 60);
-          const computedSecond =
-            String(secondCounter).length === 1
-              ? `0${secondCounter}`
-              : secondCounter;
-          const computedMinute =
-            String(minuteCounter).length === 1
-              ? `0${minuteCounter}`
-              : minuteCounter;
-          setTimeFormat(computedMinute + ":" + computedSecond);
-          sessionStorage.setItem(
-            "time-format",
-            computedMinute + ":" + computedSecond
-          );
-          setCounter(counter => counter - 1);
-          sessionStorage.setItem("active", true);
-          sessionStorage.setItem("counter", counter - 1);
-        }, 1000);
-      } else {
-        setCounter(0);
-        setTimeFormat("00:00");
-      }
+    if (!loading) {
+      console.log(Object.keys(sessionData.state).length);
+      if (Object.keys(sessionData.state).length > 0)
+        setRoundNo(Object.keys(sessionData.state).length);
     }
-    return () => {
-      clearInterval(timerRef.current);
-    };*/
-	}, [sessionData, loading]);
+  }, []);
+  const skipGameRound = () => {
+   
+    //const round = sessionData.state;
+	var data = {}
+	Object.keys(player).forEach((pl,index)=>{
+		const updates = {};
+		updates[`sessionData/${code}/state/${roundNo}/${pl}`] = 
+		{
+			
+				eye : false,
+				indivScore : 0,
+				isSelected:{
+					choice:0,
+					status:true
+				},
+				isSubmit:{
+					choice:0,
+					status:true
+				},name:Object.values(player)[index].name
+			
+	
+	}
+	update(ref(db), updates);
+	console.log(updates);
+})
 
-	const startGame = () => {
-		get(child(ref(db), `sessionData/${code}/hostProperties`))
-			.then((snapshot) => {
-				if (snapshot.exists()) {
-					console.log(snapshot.val());
-					const res = snapshot.val();
-					const updates = {};
-					updates[`sessionData/${code}/hostProperties`] = {
-						...res,
-						nextRound: true,
-					};
-					update(ref(db), updates);
-					// updates[`sessionData/${code}/state`]
-					navigate(`/round/${roundNo}`);
-				} else {
-					console.log('No data available');
-				}
-			})
-			.catch((error) => {
-				console.error(error);
-			});
-	};
+   
+  };
+  time.setSeconds(time.getSeconds() + 180); // 3 minutes timer
+  function MyTimer({ expiryTimestamp }) {
+    const {
+      seconds,
+      minutes,
+      hours,
+      days,
+      isRunning,
+      start,
+      pause,
+      resume,
+      restart,
+    } = useTimer({
+      expiryTimestamp,
+      onExpire: () => console.warn("onExpire called"),
+    });
+    useEffect(() => {
+      if (!active) {
+        //pause()
+      }
+    }, []);
 
-	const endGame = () => {
-		navigate('/gameover');
-	};
+    return (
+      <button
+        style={{ textAlign: "center" }}
+        onClick={() => {
+          if (active) {
+            setActive(false);
+            console.log(`timer ${active}`);
+            //pause()
+          } else {
+            setActive(true);
+            // resume()
+          }
+        }}
+        className="text-warning self-center text-8xl bg-black rounded-full px-3 py-3"
+      >
+        <div style={{ fontSize: "100px" }}>
+          <span>{minutes}</span>:<span>{seconds}</span>
+        </div>
+      </button>
+    );
+  }
 
-	const skipGameRound = () => {};
+  const getRoundNo = () => {
+    get(child(ref(db), `sessionData/${code}/state`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const res = Object.keys(snapshot.val());
+          console.log(res[res.length - 1]);
+          setRoundNo(Number(res[res.length - 1]) + 1);
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-	return (
-		<div className="flex flex-col items-center justify-center h-screen">
-			<div className="w-auto">
-				<h1 className="text-yellow-800 bg-yellow-400 px-3 pt-3 pb-2 rounded-lg text-md mb-0">
-					{'Waiting Arena'}{' '}
-				</h1>
-			</div>
-			<div className="bg-card bg-no-repeat bg-cover">
-				{(roundNo === 5 || roundNo === 8 || roundNo === 10) && authUser ? (
-					<div className="flex flex-col w-full justify-center items-center pt-3">
-						<h1 className="text-2xl text-warning font-bold px-4">
-							{!active
-								? 'Start the discussion for team leaders!'
-								: 'Discussion started!'}
-						</h1>
-						<button
-							onClick={() => {
-								sessionStorage.setItem('active', !active);
-								setActive(!active);
-							}}
-							className="text-warning self-center text-8xl bg-black rounded-full px-3 py-3"
-						>
-							{timeFormat}
-						</button>
-					</div>
-				) : null}
-				{authUser ? (
-					roundNo < 11 ? (
-						<div className="flex flex-row justify-center items-center py-10 px-10 w-full">
-							<div>
-								<button
-									onClick={startGame}
-									className="skip-btn btn-lg"
-								>{`Go to Day ${roundNo}`}</button>
-							</div>
-							<div>
-								<button
-									onClick={skipGameRound}
-									className="skip-btn btn-lg ml-5"
-								>{`Skip Day ${roundNo}`}</button>
-							</div>
-						</div>
-					) : (
-						<Button
-							text={`End Game`}
-							display={'bg-btn-bg-primary p-4'}
-							clickHandler={endGame}
-						/>
-					)
-				) : (
-					<div className="flex flex-col">
-						<h1 className="text-warning text-2xl font-bold px-10 pt-10">
-							Waiting for the host to start the next round
-						</h1>
-						<p className="self-center text-white my-2 text-xl">{waitingMsg}</p>
-					</div>
-				)}
-			</div>
-		</div>
-	);
+  const movetoNextRound = () => {
+    if (
+      sessionData &&
+      sessionData.hostProperties.nextRound &&
+      sessionData.hostProperties.isOver
+    ) {
+      const next = Object.keys(sessionData.state).length + 1;
+      console.log(next);
+      setTimeout(() => {
+        navigate(`/round/${next}`);
+      }, 1000);
+    }
+  };
+  useEffect(() => {
+    //console.log(auth);\
+    getRoundNo();
+    if (!authUser && !loading) {
+      movetoNextRound();
+    }
+    console.log(roundNo);
+  }, [sessionData, loading]);
+
+  const startGame = () => {
+    get(child(ref(db), `sessionData/${code}/hostProperties`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          console.log(snapshot.val());
+          const res = snapshot.val();
+          const updates = {};
+          updates[`sessionData/${code}/hostProperties`] = {
+            ...res,
+            nextRound: true,
+          };
+          update(ref(db), updates);
+          // updates[`sessionData/${code}/state`]
+          navigate(`/round/${roundNo}`);
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const endGame = () => {
+    navigate("/gameover");
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="w-auto">
+        <h1 className="text-yellow-800 bg-yellow-400 px-3 pt-3 pb-2 rounded-lg text-md mb-0">
+          {"Waiting Arena"}{" "}
+        </h1>
+      </div>
+      <div className="bg-card bg-no-repeat bg-cover">
+        {(roundNo === 5 || roundNo === 8 || roundNo === 10) && authUser ? (
+          <div className="flex flex-col w-full justify-center items-center pt-3">
+            <h1 className="text-2xl text-warning font-bold px-4">
+              {!active
+                ? "Start the discussion for team leaders!"
+                : "Discussion started!"}
+            </h1>
+
+            <MyTimer expiryTimestamp={time} />
+          </div>
+        ) : null}
+        {authUser ? (
+          roundNo < 11 ? (
+            <div className="flex flex-row justify-center items-center py-10 px-10 w-full">
+              <div>
+                <button
+                  onClick={startGame}
+                  className="skip-btn btn-lg"
+                >{`Go to Day ${roundNo}`}</button>
+              </div>
+              <div>
+                <button
+                  onClick={skipGameRound}
+                  className="skip-btn btn-lg ml-5"
+                >{`Skip Day ${roundNo}`}</button>
+              </div>
+            </div>
+          ) : (
+            <Button
+              text={`End Game`}
+              display={"bg-btn-bg-primary p-4"}
+              clickHandler={endGame}
+            />
+          )
+        ) : (
+          <div className="flex flex-col">
+            <h1 className="text-warning text-2xl font-bold px-10 pt-10">
+              Waiting for the host to start the next round
+            </h1>
+            <p className="self-center text-white my-2 text-xl">{waitingMsg}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Waiting;
